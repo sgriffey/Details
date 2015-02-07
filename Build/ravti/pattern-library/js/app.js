9231e11cf0ca7c0b027e247b985b6edce69c03aa
sgriffey <sbgriffi@gmail.com>
2015-02-07 11:08:56 -0800

    pattern library pushes

--- Build/ravti/pattern-library/js/app.js
+++ Build/ravti/pattern-library/js/app.js
@@ -1,6 +1,10 @@
 $(document).ready(function() {
-     $(".mobileNav__link").click(function() {
+    $(".mobileNav__link").click(function() {
 	  $(".mobileNav__menu").toggleClass("mobileNav__menu-is-active");
-	  return false;
 	});
+
+ 	$(".widgets__subnav-page").click(function(){
+    	$('.widgets__subnav-page .is-se').not(this).removeClass('checked');
+    $(this).toggleClass('checked');
+ })
 });
